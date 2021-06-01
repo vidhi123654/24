f63c5538f0b2bed835a405d0ddea51b84842811f
vidhi123654 <82191902+vidhi123654@users.noreply.github.com>
2021-06-01 13:49:03 -0700

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,18 @@
+class Stone{
+ 
+    constructor(){
+
+        var options={
+            "restitution":0.8,
+            "friction":0.9,
+            "density":1
+        }
+        
+        this.x=x
+        this.y=y
+        
+
+    }
+
+
+}
